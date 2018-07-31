bd5fb10d29efdaca167002ba948b7f2519cb3337
bluebeadlily@gmail.com <bluebeadlily@gmail.com>
2018-07-31 11:41:52 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is ${name}"
+end
+
+def batch_badge_creator(array_names)
+  array_names.collect do |name|
+      badge_maker(name)
+    end
+  end
+
+    def assign_rooms(speakers)
+      room=0
+      speakers.collect do |name|
+        room+=1
+        "Hello, #{name}! You'll be assigned to room #{room}!"
+      end
+    end
+
+    def printer(attendees)
+  batch_badge_creator(attendees).each do |value|
+    puts value
+  end
+end
+
+  assign_rooms(speakers).each do |value|
+    puts value
+  end
+end
+  
