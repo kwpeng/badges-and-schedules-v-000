4e064f2f6d1ebd6d86ca450c1faece246758b038
bluebeadlily@gmail.com <bluebeadlily@gmail.com>
2018-07-31 11:43:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def badge_maker(name)
-  puts "Hello, my name is ${name}"
+  puts "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(array_names)
@@ -26,4 +26,3 @@ def batch_badge_creator(array_names)
     puts value
   end
 end
-  
